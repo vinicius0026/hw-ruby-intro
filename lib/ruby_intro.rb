f719d7b9b5a807efbc51a4a552ac6d8960f5d50d
Vinicius Teixeira <vinicius0026@gmail.com>
2015-10-27 23:48:48 -0200

    all code passing

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,53 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  accumulated = 0
+  arr.each {|n| accumulated += n}
+  accumulated
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr[0] if arr.length == 1
+  arr.sort!.reverse!
+  arr[0] + arr[1]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.each_index do |i|
+    if arr.include?(n - arr[i]) and arr.find_index(n - arr[i]) != i
+      return true
+    end
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " << name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s[0] =~ /[b-df-hj-np-tv-z]/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if s =~ /[^01]/ or s.length == 0
+  s.to_i(10) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.length == 0 or price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  def price_as_string
+    "$#{format("%.2f", @price)}"
+  end
 end
